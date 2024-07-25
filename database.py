7f6116ca6ac751d1a8df365ab54558b01052af63
GonzaGomezPizarro <1912988@ucc.edu.ar>
2024-07-25 18:38:18 -0300

    ping

--- database.py
+++ database.py
@@ -1,59 +1,17 @@
 import mysql.connector
+from mysql.connector import Error
+import os
 from fastapi import HTTPException
-import requests
-from google.auth import compute_engine
-import sqlalchemy
-from google.cloud.sql.connector import Connector
-
 
 class MySQLDatabase:
-    def __init__(self, database, user, password, production=False): # '/cloudsql/frontend-430223:us-central1:mysql-server'
+    def __init__(self, database, user, password):
+        self.host = "34.70.233.252" #esto lo puedo cambiar luego     
         self.database = database
         self.user = user
         self.password = password
-        self.production = production
         self.connection = None
-        self.host = "34.70.233.252"  # para probar localmente
-        self.instance_connection_name = 'frontend-430223:us-central1:mysql-server'  # Nombre de la conexión de la instancia de Cloud SQL
 
     def connect(self):
-        if self.production:
-            self.connect_production()
-        else:
-            self.connect_local()
-
-    def connect_production(self):
-        try:
-            connector = Connector()
-
-            def getconn():
-                conn = connector.connect(
-                    self.instance_connection_name,
-                    "pymysql",
-                    user=self.user,
-                    password=self.password,
-                    db=self.database
-                )
-                return conn
-
-            pool = sqlalchemy.create_engine(
-                "mysql+pymysql://",
-                creator=getconn,
-            )
-            
-            self.connection = pool.connect()
-            print("Conexión a MySQL en producción establecida")
-            self.create_users_table_if_not_exists()
-        except Exception as e:
-            print("-------------------------------")
-            print("DATABASE ERROR")
-            print()
-            print(f"Error al conectar a MySQL en producción: {e}")
-            print()
-            print("-------------------------------")
-            raise e
-
-    def connect_local(self):
         try:
             if self.connection is None or not self.connection.is_connected():
                 self.connection = mysql.connector.connect(
@@ -62,17 +20,17 @@ class MySQLDatabase:
                     user=self.user,
                     password=self.password
                 )
-                print("Conexión a MySQL local establecida")
+                print("Conexión a MySQL establecida")
                 self.create_users_table_if_not_exists()
-        except Exception as e:
+        except Error as e:
             print("-------------------------------")
             print("DATABASE ERROR")
             print()
-            print(f"Error al conectar a MySQL local: {e}")
+            print(f"Error al conectar a MySQL: {e}")
             print()
             print("-------------------------------")
-            raise e
-
+            #raise e
+        
     def create_users_table_if_not_exists(self):
         if self.connection.is_connected() == False:
             print("BASE DE DATOS NO CONECTADA")
@@ -88,7 +46,7 @@ class MySQLDatabase:
             cursor.execute(create_table_query)
             cursor.close()
             print("Tabla 'users' creada o ya existe")
-        except Exception as e:
+        except Error as e:
             print(f"Error al crear la tabla 'users': {e}")
             raise e
 
@@ -96,55 +54,53 @@ class MySQLDatabase:
         if self.connection and self.connection.is_connected():
             self.connection.close()
             print("Conexión a MySQL cerrada")
-
+    
     def get_users(self):
         try:
-            print("PING")
             if self.connection.is_connected() == False:
-                print("BASE DE DATOS NO CONECTADA")
-                raise HTTPException(500, "FALLO EN LA CONEXIÓN CON LA BASE DE DATOS")
-
+                print ("BASE DE DATOS NO CONECTADA")
+                raise HTTPException( 500, "FALLO EN LA CONEXCION CON LA BASE DE DATOS")
+            
             cursor = self.connection.cursor(dictionary=True)
             cursor.execute("SELECT * FROM users")
             users = cursor.fetchall()
             cursor.close()
             return users
-        except Exception as e:
-            print(f"Exception al obtener usuarios: {e}")
+        except Error as e:
+            print(f"Error al obtener usuarios: {e}")
             raise e
 
     def get_user(self, username: str):
         try:
             if self.connection.is_connected() == False:
-                print("BASE DE DATOS NO CONECTADA")
-                raise HTTPException(500, "FALLO EN LA CONEXIÓN CON LA BASE DE DATOS")
-
+                print ("BASE DE DATOS NO CONECTADA")
+                raise HTTPException( 500, "FALLO EN LA CONEXCION CON LA BASE DE DATOS")
+            
             cursor = self.connection.cursor(dictionary=True)
             cursor.execute("SELECT * FROM users WHERE username = %s", (username,))
             user = cursor.fetchone()
             cursor.close()
             return user
-        except Exception as e:
-            print(f"Exception al obtener usuario: {e}")
+        except Error as e:
+            print(f"Error al obtener usuario: {e}")
             raise e
 
-    def insert_user(self, username: str, password: str):
+    def insert_user(self, username: str,  password: str):
         try:
             if self.connection.is_connected() == False:
-                print("BASE DE DATOS NO CONECTADA")
-                raise HTTPException(500, "FALLO EN LA CONEXIÓN CON LA BASE DE DATOS")
-
+                print ("BASE DE DATOS NO CONECTADA")
+                raise HTTPException( 500, "FALLO EN LA CONEXCION CON LA BASE DE DATOS")
+                       
             cursor = self.connection.cursor()
             cursor.execute("INSERT INTO users (username, password) VALUES (%s, %s)", (username, password))
             self.connection.commit()
             user_id = cursor.lastrowid
             cursor.close()
             return user_id
-        except Exception as e:
-            print(f"Exception al insertar usuario: {e}")
+        except Error as e:
+            print(f"Error al insertar usuario: {e}")
             self.connection.rollback()
             raise e
-
+        
 # Uso de la clase
-db = MySQLDatabase(database='Usuarios', user='root', password='roott', production=True)
-db.connect()
+db = MySQLDatabase(database='Usuarios', user='root', password='roott')
