26701d0d5d2991748349876bbb0e8b2ee80e4ff7
GonzaGomezPizarro <1912988@ucc.edu.ar>
2024-07-25 21:01:12 -0300

    test cambio de textooo

--- main.py
+++ main.py
@@ -46,7 +46,7 @@ async def get_users():
     try:
         users = controller.GetUsers()
         if users:
-            return "LA RE CONCHA DE TU HRMANAAAAAAAAAAAAA"#users
+            return users
         else:
             raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="No user found in database")
     except HTTPException as xp:
