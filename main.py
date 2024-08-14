b74d469225d92d91f0d87f28a34fa14ed668465c
GonzaGomezPizarro <1912988@ucc.edu.ar>
2024-08-14 16:46:34 -0300

    PING PING - ACTUALIZADO CORRECTO DE LA IMAGEN

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ app.add_middleware(
 @app.get("/")
 async def hello_world():
     return {
-        "title": "EN ESTA VERISION ANDA EL ACTUALIZADO DE LA IMAGEN CORRECTAMENTEEEEEE"
+        "title": "EN ESTA VERISION ANDA EL ACTUALIZADO DE LA IMAGEN CORRECTAMENTEEEEE"
     }
 
 
